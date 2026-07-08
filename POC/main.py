ed33686106422cfd0682a55ba4cccb62173400f9
sidharth024gb <124123122+sidharth024gb@users.noreply.github.com>
2026-07-08 13:26:58 +0100

    Altered Folder Structure - Moved files POC folder

--- POC/main.py
+++ POC/main.py
@@ -2,10 +2,12 @@ from agent import train_agent
 import time
 
 if __name__ == "__main__":
+    # Train Agent
     start_time = time.perf_counter()
     train_agent()
     end_time = time.perf_counter()
 
+    # Calculate the Duratio of the Agent Training
     duration = end_time - start_time
     hours, reminder = divmod(duration, 3600)
     minutes, seconds = divmod(reminder, 60)
